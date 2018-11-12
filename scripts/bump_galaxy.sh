9a2fb610bf0b150a915dba5d3bb3260502fec8ae
Andy Liu <andyliuliming@outlook.com>
2018-11-12 22:01:14 +0800

    prepare for the moodle project.

--- scripts/bump_galaxy.sh
+++ scripts/bump_galaxy.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-# go get github.com/mjibson/esc
-# pushd $DIR/..
-# ansible-galaxy install geerlingguy.composer
-# ansible-galaxy install geerlingguy.php
-# ansible-galaxy install geerlingguy.glusterfs
-# ansible-galaxy install geerlingguy.firewall
-# ansible-galaxy install geerlingguy.mysql
-# ansible-galaxy install geerlingguy.redis
-# ansible-galaxy install geerlingguy.php-redis
-# ansible-galaxy install geerlingguy.php-mysql
-# ansible-galaxy install nginxinc.nginx
-# popd
+go get github.com/mjibson/esc
+pushd $DIR/../pkg/apis
+  ansible-galaxy install geerlingguy.composer
+  ansible-galaxy install geerlingguy.php
+  ansible-galaxy install geerlingguy.glusterfs
+  ansible-galaxy install geerlingguy.firewall
+  ansible-galaxy install geerlingguy.git
+  ansible-galaxy install geerlingguy.mysql
+  ansible-galaxy install geerlingguy.redis
+  ansible-galaxy install geerlingguy.php-redis
+  ansible-galaxy install geerlingguy.php-mysql
+  ansible-galaxy install nginxinc.nginx
+popd
 
 pushd $DIR/../pkg/apis
-go generate
+  go generate
 popd
 
